bf50a92d8f1565f51068135e707fd4d16b4f487d
katiez07 <49448727+katiez07@users.noreply.github.com>
2022-02-05 01:49:55 -0500

    fixed styling unable to apply to p-button

--- libs/header/src/lib/components/header.component.ts
+++ libs/header/src/lib/components/header.component.ts
@@ -6,19 +6,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
             <div style="display: flex; padding: 10px">
                 <h1 routerLink="/" style="cursor: pointer">Mountain SOL</h1>
                 <span style="flex: 1 1 auto"></span>
-                <p-button
-                    style="margin-right: 4px"
-                    routerLink="/calendar/classes"
-                    label="Calendar Demo"
-                    styleClass="p-button-success"
-                ></p-button>
-                <p-button
-                    style="margin-right: 4px"
-                    routerLink="/report"
-                    label="Report Demo"
-                    styleClass="p-button-help"
-                ></p-button>
-                <sol-user-button></sol-user-button>
+                <div style="margin-right: 4px">
+                    <p-button
+                        routerLink="/calendar/classes"
+                        label="Calendar Demo"
+                        styleClass="p-button-success"
+                    ></p-button>
+                </div>
+                <div style="margin-right: 4px">
+                    <p-button
+                        routerLink="/report"
+                        label="Report Demo"
+                        styleClass="p-button-help"
+                    ></p-button>
+                    <sol-user-button></sol-user-button>
+                </div>
             </div>
         </header>
         <router-outlet></router-outlet>`,
