322afa57cf0abf76fd42ba1426bf8cd81fe0cf73
David <25187726+david-shortman@users.noreply.github.com>
2022-05-27 23:11:48 -0400

    add functions to firebase.json

--- apps/functions/src/main.ts
+++ apps/functions/src/main.ts
@@ -5,7 +5,6 @@ import { FirebasePdf } from '@sol/pdf/firebase';
 import { DatabaseUtility } from '@sol/firebase/database';
 import { RosterReportGenerator } from '@sol/student/reports';
 import { ClassEmailGenerator } from '@sol/student/reports';
-import * as functions from 'firebase-functions';
 
 export const roster = HttpUtility.aGetEndpoint(async (request, response) => {
     AuthUtility.validateIsAdmin(request, response);
