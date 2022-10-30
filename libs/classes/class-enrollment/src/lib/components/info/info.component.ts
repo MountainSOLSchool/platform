8b4e72dda0a6dcee2b016f0e012fd8b23fa31f88
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2022-10-30 17:44:38 -0400

    Co-authored-by: David <david-shortman@users.noreply.github.com>

--- libs/classes/class-enrollment/src/lib/components/info/info.component.ts
+++ libs/classes/class-enrollment/src/lib/components/info/info.component.ts
@@ -2,6 +2,15 @@ import { Component, inject, Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { tap } from 'rxjs';
 import { EnrollmentWorkflowStore } from '../enrollment-workflow/enrollment-workflow.store';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { FieldsetModule } from 'primeng/fieldset';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+
 
 @Injectable()
 class InfoStore extends ComponentStore<{ nil: null }> {
@@ -20,7 +29,16 @@ class InfoStore extends ComponentStore<{ nil: null }> {
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [
+        InputTextModule,
+        CalendarModule,
+        ButtonModule,
+        InputMaskModule,
+        FieldsetModule,
+        RippleModule,
+        InputTextareaModule,
+        InputNumberModule
+    ],
     templateUrl: './info.component.html',
     styleUrls: ['./info.component.css'],
     providers: [InfoStore],
