899762a007e10df52d2c7181a76da335a4b1226b
katiez07 <49448727+katiez07@users.noreply.github.com>
2022-11-28 19:08:48 -0500

    updated student info to include parents guardians

--- libs/classes/class-enrollment/src/lib/class-enrollment.ts
+++ libs/classes/class-enrollment/src/lib/class-enrollment.ts
@@ -5,6 +5,7 @@ import { ConfirmationComponent } from './components/confirmation/confirmation.co
 import { InfoComponent } from './components/info/info.component';
 import { ClassEnrollmentComponent } from './components/enrollment-workflow/enrollment-workflow.component';
 import { AccountComponent } from './components/account/account.component';
+import { MedicalComponent } from './components/medical/medical.component';
 
 export const enrollmentRoutes: Route[] = [
     {
@@ -14,6 +15,7 @@ export const enrollmentRoutes: Route[] = [
             { path: '', redirectTo: 'classes', pathMatch: 'full' },
             { path: 'classes', component: ClassesComponent },
             { path: 'info', component: InfoComponent },
+            { path: 'medical', component: MedicalComponent },
             { path: 'account', component: AccountComponent },
             { path: 'checkout', component: CheckoutComponent },
             { path: 'confirmation', component: ConfirmationComponent },
