d127fcb1e8941f6ad4cd98c6f3846f89fade2dde
David <25187726+david-shortman@users.noreply.github.com>
2022-12-08 23:28:18 -0500

    additional error handling

--- libs/firebase/functions/src/lib/utilities/functions.utility.ts
+++ libs/firebase/functions/src/lib/utilities/functions.utility.ts
@@ -43,27 +43,33 @@ class FunctionBuilder<SecretNames extends string> {
             .runWith({ secrets: Object.values(this.secrets) })
             .https.onRequest(async (request, response) => {
                 cors(request, response, async () => {
+                    console.log('Running a function', request.path);
                     this.roles.forEach((role) => {
                         AuthUtility.validateRole(request, response, role);
                     });
-                    handler(
-                        request,
-                        {
-                            ...response,
-                            status: (code: number) => response.status(code),
-                            send: (data: unknown) => {
-                                response.send({ data });
-                            },
-                        } as functions.Response,
-                        Object.fromEntries(
-                            Object.entries(this.secrets).map(
-                                ([key, secret]: [string, SecretParam]) => [
-                                    key,
-                                    secret.value(),
-                                ]
+                    try {
+                        handler(
+                            request,
+                            {
+                                ...response,
+                                status: (code: number) => response.status(code),
+                                send: (data: unknown) => {
+                                    response.send({ data });
+                                },
+                            } as functions.Response,
+                            Object.fromEntries(
+                                Object.entries(this.secrets).map(
+                                    ([key, secret]: [string, SecretParam]) => [
+                                        key,
+                                        secret.value(),
+                                    ]
+                                )
                             )
-                        )
-                    );
+                        );
+                    } catch (error) {
+                        console.error(error);
+                        response.status(500).send({ error });
+                    }
                 });
             });
     }
