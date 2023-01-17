d10e01c4e4ad237414fe500cb77625b16174cca0
David <25187726+david-shortman@users.noreply.github.com>
2023-01-16 22:54:26 -0500

    search and filter adjustments

--- libs/classes/class-enrollment/src/lib/components/classes/class-list/class-list.component.ts
+++ libs/classes/class-enrollment/src/lib/components/classes/class-list/class-list.component.ts
@@ -2,8 +2,16 @@ import { Component, inject, Output } from '@angular/core';
 import {
     BehaviorSubject,
     combineLatest,
+    debounceTime,
+    delay,
+    filter,
     map,
+    merge,
+    ObservedValueOf,
+    of,
+    shareReplay,
     switchMap,
+    tap,
     withLatestFrom,
 } from 'rxjs';
 import { EnrollmentWorkflowStore } from '../../enrollment-workflow/enrollment-workflow.store';
@@ -19,6 +27,9 @@ import { LetModule } from '@rx-angular/template/let';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { TagModule } from 'primeng/tag';
 import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { IfModule } from '@rx-angular/template/if';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
     standalone: true,
@@ -34,6 +45,9 @@ import { ToggleButtonModule } from 'primeng/togglebutton';
         SelectButtonModule,
         TagModule,
         ToggleButtonModule,
+        ProgressSpinnerModule,
+        IfModule,
+        DropdownModule,
     ],
     selector: 'sol-class-picker',
     templateUrl: './class-list.component.html',
@@ -47,6 +61,24 @@ export class ClassesComponent {
 
     private search$ = new BehaviorSubject('');
 
+    // emits true after a search has been performed, and emits false 500ms afterwards
+
+    private gradeFilter$ = new BehaviorSubject<[] | [number, number]>([]);
+
+    private isFakeSearchStarted$ = combineLatest([
+        this.search$,
+        this.gradeFilter$,
+    ]).pipe(map(() => true));
+
+    readonly isFakeSearchLoading$ = merge(
+        this.isFakeSearchStarted$,
+        this.isFakeSearchStarted$.pipe(
+            delay(800),
+            map(() => false)
+        ),
+        of(false)
+    ).pipe(tap((x) => console.log('isFakeSearchLoading$', x)));
+
     selectedClassIds$ = this.workflow.select(
         (state) => state.enrollment.selectedClasses
     );
@@ -55,13 +87,8 @@ export class ClassesComponent {
         map((selectedClasses) => selectedClasses.length > 0)
     );
 
-    readonly filterOptions = [
-        { name: 'All', value: [] },
-        { name: 'Grades 2-5', value: [2, 5] },
-        { name: 'Grades 6-12', value: [6, 12] },
-    ];
-
     classes$ = this.classListService.getFutureClasses().pipe(
+        shareReplay(),
         map((classes) =>
             classes
                 .map((c) => {
@@ -98,11 +125,59 @@ export class ClassesComponent {
         )
     );
 
-    classesResult$ = combineLatest([this.classes$, this.search$]).pipe(
-        map(([classes, search]) => {
-            return classes.filter((c) =>
-                c.title.toLowerCase().includes(search.toLowerCase())
-            );
+    readonly filterOptions$ = this.classes$.pipe(
+        map((classes) =>
+            [
+                ...classes
+                    .reduce(
+                        (map, c) =>
+                            map.set(
+                                `Grades ${c.gradeRangeStart}-${c.gradeRangeEnd}`,
+                                c
+                            ),
+                        new Map<
+                            `Grades ${number}-${number}`,
+                            ObservedValueOf<typeof this.classes$>[number]
+                        >()
+                    )
+                    .entries(),
+            ]
+                .sort(([a], [b]) => a.localeCompare(b))
+                .map(([label, { gradeRangeStart, gradeRangeEnd }]) => ({
+                    name: label,
+                    value: [gradeRangeStart, gradeRangeEnd],
+                }))
+        )
+    );
+
+    classesResult$ = combineLatest([
+        this.classes$,
+        this.search$,
+        this.gradeFilter$,
+    ]).pipe(
+        map(([classes, search, gradeFilter]) => {
+            const searched = classes.filter((c) => {
+                return (
+                    c.title.toLowerCase().includes(search.toLowerCase()) ||
+                    c.description
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                    c.location.toLowerCase().includes(search.toLowerCase()) ||
+                    c.classDateTimes
+                        .toLowerCase()
+                        .includes(search.toLowerCase())
+                );
+            });
+            const [startGrade, endGrade] = gradeFilter;
+            const filtered =
+                startGrade && endGrade
+                    ? searched.filter(
+                          (c) =>
+                              c.gradeRangeStart === startGrade &&
+                              c.gradeRangeEnd === endGrade
+                      )
+                    : searched;
+            return filtered;
         })
     );
 
@@ -136,5 +211,10 @@ export class ClassesComponent {
 
     searchChange(search: string) {
         this.search$.next(search);
+    }
+
+    filterChange(filter: [] | [number, number]) {
+        console.log(filter);
+        this.gradeFilter$.next(filter);
     }
 }
