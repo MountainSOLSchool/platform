c17b4dcf6e44320f4103324d3298deced5435fdc
David <25187726+david-shortman@users.noreply.github.com>
2023-02-08 00:10:36 -0500

    better vertical scroll support

--- libs/classes/class-enrollment/src/lib/components/enrollment-workflow/vertical-steps.directive.ts
+++ libs/classes/class-enrollment/src/lib/components/enrollment-workflow/vertical-steps.directive.ts
@@ -9,20 +9,25 @@ import { MatStepper } from '@angular/material/stepper';
 export class MatVerticalStepperScrollerDirective {
     constructor(private stepper: MatStepper) {}
 
+    private firstAnimation = true;
+
     @HostListener('animationDone')
     selectionChanged() {
-        if (this.stepper.orientation === 'vertical') {
-            const stepId = this.stepper._getStepLabelId(
-                this.stepper.selectedIndex
-            );
-            const stepElement = document.getElementById(stepId);
-            if (stepElement) {
-                stepElement.scrollIntoView({
-                    block: 'start',
-                    inline: 'nearest',
-                    behavior: 'smooth',
-                });
+        if (!this.firstAnimation) {
+            if (this.stepper.orientation === 'vertical') {
+                const stepId = this.stepper._getStepLabelId(
+                    this.stepper.selectedIndex
+                );
+                const stepElement = document.getElementById(stepId);
+                if (stepElement) {
+                    stepElement.scrollIntoView({
+                        block: 'start',
+                        inline: 'nearest',
+                        behavior: 'smooth',
+                    });
+                }
             }
         }
+        this.firstAnimation = false;
     }
 }
