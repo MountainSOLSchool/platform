7c259fd721b47067741a0742c658250c95040261
David <25187726+david-shortman@users.noreply.github.com>
2023-03-16 21:30:46 -0400

    setup portal2

--- apps/portal-react/pages/_app.tsx
+++ apps/portal-react/pages/_app.tsx
@@ -1,6 +1,17 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp({
+    apiKey: 'AIzaSyBxv66X_Ye4MXI5lt8Sjc1xz88rdWJJ0ho',
+    authDomain: 'mountain-sol-platform.web.app',
+    projectId: 'mountain-sol-platform',
+    storageBucket: 'mountain-sol-platform.appspot.com',
+    messagingSenderId: '319228048592',
+    appId: '1:319228048592:web:2d418795ca948ba2665ad5',
+    measurementId: 'G-QN03ENCDDC',
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
     return (
