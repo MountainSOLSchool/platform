371aef88a4e14d0224b0d07956e78b9fd5b6dea8
David <25187726+david-shortman@users.noreply.github.com>
2023-05-26 20:21:49 -0400

    solve for tshirt uniqueness

--- libs/firebase/student/reports/src/lib/utilities/student-tshirts.generator.ts
+++ libs/firebase/student/reports/src/lib/utilities/student-tshirts.generator.ts
@@ -2,16 +2,28 @@ import {
     SemesterRepository,
     SUMMER_2023_SEMESTER,
 } from '@sol/classes/repository';
+import { StudentDbEntry } from '@sol/student/domain';
 
 export class StudentTshirtsGenerator {
     static async createTshirtList() {
         const students = await SemesterRepository.of(
             SUMMER_2023_SEMESTER
         ).students.getEnrolled();
-        return students.map((student) => ({
-            firstName: student.first_name,
-            lastName: student.last_name,
-            size: student.tshirt_size,
-        }));
+        const uniqueStudentsByNameAndBirthdate: Map<string, StudentDbEntry> =
+            students.reduce(
+                (unique, student) =>
+                    unique.set(
+                        `${student.first_name}${student.last_name}${student.birth_date}`,
+                        student
+                    ),
+                new Map<string, StudentDbEntry>()
+            );
+        return Array.from(uniqueStudentsByNameAndBirthdate.values()).map(
+            (student) => ({
+                firstName: student.first_name,
+                lastName: student.last_name,
+                size: student.tshirt_size,
+            })
+        );
     }
 }
