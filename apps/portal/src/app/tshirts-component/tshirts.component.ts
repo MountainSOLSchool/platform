fb31786004fa2dfacce75d9468f0db1245ce098a
David <25187726+david-shortman@users.noreply.github.com>
2023-05-26 20:12:33 -0400

    fix up tshirt report

--- apps/portal/src/app/tshirts-component/tshirts.component.ts
+++ apps/portal/src/app/tshirts-component/tshirts.component.ts
@@ -4,11 +4,12 @@ import { map, shareReplay } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { TableModule } from 'primeng/table';
+import { LetModule } from '@rx-angular/template/let';
 
 @Component({
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CommonModule, ProgressBarModule, TableModule],
+    imports: [CommonModule, ProgressBarModule, TableModule, LetModule],
     templateUrl: './tshirts.component.html',
 })
 export class TshirtsComponent {
@@ -33,4 +34,17 @@ export class TshirtsComponent {
             ),
             shareReplay(1)
         );
+
+    sizeCounts$ = this.students$.pipe(
+        map((students) => {
+            return ['XS', 'S', 'M', 'L', 'XL'].map((size) => ({
+                size,
+                count: this.getSizeCount(size, students),
+            }));
+        })
+    );
+
+    getSizeCount(size: string, students: Array<{ size: string }>) {
+        return students.filter((student) => student.size === size).length;
+    }
 }
