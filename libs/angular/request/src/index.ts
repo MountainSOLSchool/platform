c64c13a0053eb2d2b298f301655b74c1c8cc9c60
David <25187726+david-shortman@users.noreply.github.com>
2023-11-04 15:55:56 -0500

    remove component

--- libs/angular/request/src/index.ts
+++ libs/angular/request/src/index.ts
@@ -1,6 +1,5 @@
 export { RequestState } from './lib/models/requested.type';
 export { Requested } from './lib/models/requested.type';
-export { RequestedComponent } from './lib/components/requested.component';
 export { SolLoadedDirective } from './lib/directives/loaded-state.directive';
 export { SolLoadingDirective } from './lib/directives/loading-state.directive';
 export { SolErrorDirective } from './lib/directives/error-state.directive';
