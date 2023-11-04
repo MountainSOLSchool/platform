40ccbc2fbf902ff337bab336e9f3d87291f39419
David <25187726+david-shortman@users.noreply.github.com>
2023-11-04 15:37:07 -0500

    update import

--- libs/angular/account/src/lib/services/account-enrollments.service.ts
+++ libs/angular/account/src/lib/services/account-enrollments.service.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { FirebaseFunctionsService } from '@sol/firebase/functions-api';
+import { SemesterEnrollment } from '@sol/classes/domain';
 
 @Injectable({
     providedIn: 'root',
