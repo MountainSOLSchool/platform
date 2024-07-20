c2505bbf73d8edc37b96a353630ed1b7286b10b8
David <25187726+david-shortman@users.noreply.github.com>
2024-07-19 21:10:12 -0400

    add infrastructure module

---
+++ apps/admin-portal-client-infrastructure/src/main.tf
@@ -0,0 +1,71 @@
+variable "GITHUB_TOKEN" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+terraform {
+  cloud {
+    organization = "mountain-sol-platform"
+
+    workspaces {
+      name = "admin-portal"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_amplify_app" "admin_portal" {
+  name       = "admin_portal"
+  repository = "https://github.com/MountainSOLSchool/platform"
+  access_token = var.GITHUB_TOKEN
+
+  environment_variables = {
+    AMPLIFY_MONOREPO_APP_ROOT = "apps/portal-react"
+  }
+
+  build_spec = <<-EOT
+    version: 1
+    applications:
+      - appRoot: apps/portal-react
+        frontend:
+          phases:
+            build:
+              commands:
+                - cd ../
+                - npm ci
+                - npx nx run portal-react:build:production
+          artifacts:
+            baseDirectory: ../dist/test-app/.next
+            files:
+              - '**/*'
+          cache:
+            paths:
+              - node_modules/**/*
+  EOT
+
+  platform = "WEB_COMPUTE"
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.admin_portal.id
+  branch_name = "main"
+  enable_auto_build = false
+
+  stage     = "PRODUCTION"
+}
+
+resource "aws_amplify_webhook" "main" {
+  app_id      = aws_amplify_app.admin_portal.id
+  branch_name = aws_amplify_branch.main.branch_name
+  description = "triggermain"
+}
