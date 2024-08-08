e41fe21983e9f7a2c31dabd0320d624112198502
katiez07 <49448727+katiez07@users.noreply.github.com>
2024-08-08 16:24:45 -0500

    fix: unblock adding student portal changes (#142)

--- apps/student-portal/components/Units/UnitSelectionTool.tsx
+++ apps/student-portal/components/Units/UnitSelectionTool.tsx
@@ -1,5 +1,12 @@
-export function UnitSelectionTool() {
-    return <div>-- unit selection tool --</div>;
+export function UnitSelectionTool(props) {
+    return (
+        <div>
+            Behold all of the Mountain SOL Units!
+            <div>Click on each unit to see the unit description.</div>;
+        </div>
+        // for each section, list units
+        // each unit is clickable and opens up the description.
+    );
 }
 
 export default UnitSelectionTool;
