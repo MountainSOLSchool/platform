f7a12c2558d3ddb1831b8f85e6f555438a9848e9
David <25187726+david-shortman@users.noreply.github.com>
2024-08-11 14:29:03 -0400

    feat: paths and unit endpoint (#145)

--- apps/student-portal/functions/firebase-functions.ts
+++ apps/student-portal/functions/firebase-functions.ts
@@ -4,21 +4,15 @@ import {
     httpsCallable,
 } from 'firebase/functions';
 import { StudentDbEntry } from '@sol/student/domain';
+import { PathDbEntry, UnitDbEntry } from '@sol/classes/domain';
 
 let _functions: ReturnType<typeof getFunctions> | undefined;
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export class FirebaseFunctions {
     private static get functions() {
-        const functions =
-            _functions ??
-            (() => {
-                const functions = getFunctions();
-                // TODO: remove this when we are done testing
-                connectFunctionsEmulator(functions, 'localhost', 5001);
-                _functions = functions;
-                return functions;
-            })();
-        _functions = functions;
+        _functions ??= getConfiguredFunctions();
         return _functions;
     }
 
@@ -31,4 +25,26 @@ export class FirebaseFunctions {
         >(this.functions, 'allStudents');
         return allStudentsFn({ fields }).then((result) => result.data.students);
     }
+
+    static getFullUnitsAndPaths(): Promise<{
+        paths: Array<PathDbEntry>;
+        units: Record<string, UnitDbEntry>;
+    }> {
+        const getPathsFn = httpsCallable<
+            void,
+            {
+                paths: Array<PathDbEntry>;
+                units: Record<string, UnitDbEntry>;
+            }
+        >(this.functions, 'fullUnitsAndPaths');
+        return getPathsFn().then((result) => result.data);
+    }
+}
+
+function getConfiguredFunctions() {
+    const functions = getFunctions();
+    if (!isProduction) {
+        connectFunctionsEmulator(functions, 'localhost', 5001);
+    }
+    return functions;
 }
